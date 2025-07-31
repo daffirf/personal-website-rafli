22884f2da2d82400c39c5c4615386e7d5887cd5d
daffirf <rafli.fajri@gmail.com>
2025-08-01 06:30:41 +0700

    fix: escape double quotes in Testimonials

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -7,7 +7,7 @@ const testimonials = [
     name: "Nia Marana",
     role: "CEO, Maraneea Shop",
     quote:
-      <p>Rafli is a great full stack developer. He knows what to do, tactical, fast, precise, and always delivers under pressure.</p>
+      <p>Rafli is the best designer I've ever worked with. He’s a real “game-changer”.</p>
   },
 ];
 
